f1e801ee89d3d16f5645c2060bd2e2c0d5ab6f62
Daniel Usakowski <dusak2000@gmail.com>
2026-07-01 16:48:27 -0400

    add DiceRoll class with rollDice and updateDisplay functions

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+//main player bidding section event listeners
+
 const menuArrow = document.getElementById('pop-up');
 const dieBid = document.getElementById('die-bid');
 const menuPopup = document.getElementById('menu');
-const menuDice = document.querySelectorAll('[data-die]');
+const menuDice = document.querySelectorAll('[data-menu-die]');
 const minusButton = document.getElementById('minus');
 const plusButton = document.getElementById('plus');
 const numberBid = document.getElementById('display');
@@ -13,9 +15,9 @@ menuArrow.addEventListener('click', () => {
 menuDice.forEach((die) => {
     die.addEventListener('click', () => {
         const dieFace = die.innerHTML;
-        const valueOfDieBid = die.dataset.die;
+        const valueOfDieBid = die.dataset.menuDie;
         dieBid.innerHTML = dieFace;
-        dieBid.dataset.die = valueOfDieBid;
+        dieBid.dataset.menuDie = valueOfDieBid;
         menuPopup.classList.toggle('hidden');     
     });
 });
@@ -40,4 +42,55 @@ plusButton.addEventListener('click', () => {
 
 
 const valueOfNumberBid = numberBid.textContent;
-const valueOfDieBid = dieBid.dataset.die;
+const valueOfDieBid = dieBid.dataset.menuDie;
+
+//rolling dice
+
+class DiceRoll {
+    constructor(numOfDice) {
+        this.numOfDice = numOfDice;
+        this.rollDice(); 
+    }
+    static dicePool = [
+        {value: 'wild', display: 'fa-solid fa-square-virus'},
+        {value: 'two', display: 'fa-solid fa-dice-two'}, 
+        {value: 'three', display: 'fa-solid fa-dice-three'}, 
+        {value: 'four', display: 'fa-solid fa-dice-four'}, 
+        {value: 'five', display: 'fa-solid fa-dice-five'}, 
+        {value: 'six', display: 'fa-solid fa-dice-six'}]; 
+    rollDice = () => {
+       this.diceRolledValues = [];
+       this.diceRolledFaces = [];
+       for (let i = 0; i < this.numOfDice; i++) {
+        const randomIndex = Math.floor(Math.random() * 6);
+        const dieRolled = DiceRoll.dicePool[randomIndex];
+        this.diceRolledValues.push(dieRolled.value);
+        this.diceRolledFaces.push(dieRolled.display);
+       }
+    }
+    updateDisplay = (playerName, boxName) => {
+        const diceArray  =  Array.from(document.querySelectorAll(playerName));
+        const boxArray = Array.from(document.querySelectorAll(boxName));
+        for (let i = 0; i < this.numOfDice; i++) {
+            diceArray[i].className = this.diceRolledFaces[i];
+        }
+    }
+}
+
+
+
+
+ 
+
+const rollButton = document.getElementById('roll');
+rollButton.addEventListener('click', () => {
+    const mainPlayerRoll = new DiceRoll(5);
+    mainPlayerRoll.updateDisplay('.main-player i', '.main-player .die');
+    const player1Roll = new DiceRoll(5);
+    player1Roll.updateDisplay('#player-1 i', '#player-1 .die');
+    const player2Roll = new DiceRoll(5);
+    player2Roll.updateDisplay('#player-2 i', '#player-2 .die');
+    const player3Roll = new DiceRoll(5);
+    player3Roll.updateDisplay('#player-3 i', '#player-3 .die');
+
+})
